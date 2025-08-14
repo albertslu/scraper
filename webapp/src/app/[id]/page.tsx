816f651487dde7e6eb9422bcc97b5b2f82fcb902
al43595 <70409085+al43595@users.noreply.github.com>
2025-08-14 14:30:10 -0700

    Create page.tsx

---
+++ webapp/src/app/[id]/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import Home from '../page'
+
+export default function JobDeepLinkPage() {
+  // Reuse the root page; it already reads the jobId from the path
+  return <Home />
+}
+
+
